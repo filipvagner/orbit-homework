6240162ac80cc53ecd7b5f5e382d788f7376d5fc
filipvagner <filipvagner@powershellatnight.onmicrosoft.com>
2024-09-09 16:55:59 +0200

    Added TF init

---
+++ infra/terraform/terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+
+  backend "local" {}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "< 4.0.0"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+  }
+
+  required_version = "1.9.0"
+}
